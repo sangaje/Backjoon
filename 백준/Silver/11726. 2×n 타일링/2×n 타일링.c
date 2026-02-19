c3e95bc28e7d99145baa23144610435579dce22c
산에사는가재 <36144536+sangaje@users.noreply.github.com>
2026-02-19 11:24:02 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int N, *dp;
+    scanf("%d", &N);
+
+    dp = malloc(sizeof(int) * (N + 1));
+    dp[0] = 1;
+    dp[1] = 1;
+
+    for (int i = 2; i <= N; i++)
+        dp[i] = (dp[i - 2] + dp[i - 1]) % 10007;
+
+    printf("%d\n", dp[N]);
+
+    free(dp);
+    return 0;
+}
